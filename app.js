37e8de2ae435ab3ef99555468cb2929cf5d75efa
lucero <moreta.concepcion@gmail.com>
2023-04-18 16:32:35 -0400

    - Termine el templae de la factura - Agregue Email.js para realizar el envio de correos con adjuntos.

--- app.js
+++ app.js
@@ -1,68 +1,66 @@
 import express from 'express';
 import clients_data  from './factura.js'
-
 import puppeteer from 'puppeteer';
-import pug from 'pug'
-import fs from 'fs'
+
  
-    let factura = clients_data
-    const recursos = {
-       data: factura.items,
-       css: '/style.css'
 
-    }
-
-    
   
-  
-      
-    function compilePugToHtml(file) {
-      return pug.renderFile(file, recursos);
-    }
-
 
     (async () => {
-      const browser = await puppeteer.launch({ args: ['--disable-dev-shm-usage'] });
+      const browser = await puppeteer.launch();
       const page = await browser.newPage();
     
-      // Leer el archivo Pug y compilarlo a HTML
-      const html = await compilePugToHtml('./views/index.pug');
+      // Navigate to the page with the image
+      await page.goto('http://localhost:3000/template');
     
-      // Cargar los estilos CSS y aplicarlos al PDF generado
-      const css = fs.readFileSync('./public/style.css', 'utf8');
-    
-     
-      await page.setContent(html);
-      await page.addStyleTag({content: css});
- 
-
-    
-      // Generar el PDF y guardarlo en disco
-      await page.pdf({path: 'ejemplo.pdf', printBackground: true});
+      // Generate the PDF with the image included
+      await page.pdf({
+        path: 'my-pdf-file.pdf',
+        width: '820px',
+        height: '1370px',
+        printBackground: true,
+      });
     
       await browser.close();
     })();
     
+    
+    
+    
+    let factura = clients_data
+    const recursos = {
+       data: factura.items,
+       css: '/style.css',
+       imgHeader: '/img/factura-header.jpg'
+
+    }
+
+    const app = express()
+    const port = 3000
+
+    app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
+
+    app.set('views', './views')
+
+    app.set('view engine', 'pug')
+
+    app.use(express.static('public'))
+
+    app.get('/template', (req, res) => {
+        res.render('index.pug', recursos ); 
+    });
+    
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`)
+    })
+
+
+   
+   
+
   
 
 
-                const app = express()
-                const port = 3000
-
-                app.get('/', (req, res) => {
-                  res.send('Hello World!')
-                })
-
-                app.set('views', './views')
-
-                app.set('view engine', 'pug')
-
-                app.use(express.static('public'))
-
-                app.get('/template', (req, res) => {
-                    res.render('index.pug', recursos ); 
-                });
-
-                  app.listen(port, () => {
-                    console.log(`listening on port ${port}`)
-                  })
+        
--- factura.js
+++ factura.js
@@ -1,9 +1,8 @@
 import fetch  from 'node-fetch';
 
-
-
-const response = await fetch('https://webservice.caasd.gob.do:8443/api/factura?id_tipo_trans=FS&id_documento=3283122');
+const response = await fetch('https://webservice.caasd.gob.do:8443/api/factura?id_tipo_trans=FS&id_documento=3512084');
 const clients_data = await response.json();
+
 
 
 export default clients_data
