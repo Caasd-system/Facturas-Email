e7018561ea29490ea39325a1c00ae623c585a4aa
lucero <moreta.concepcion@gmail.com>
2023-04-25 15:03:32 -0400

    Trabaje en la logica y elimine archivos

--- app.js
+++ app.js
@@ -1,66 +1,86 @@
 import express from 'express';
-import clients_data  from './factura.js'
+import { factura_data }  from './factura.js'
+import clients_data  from './Clientes.js';
 import puppeteer from 'puppeteer';
 
- 
 
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+});
+
+app.set('views', './views')
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+
+
+async function procesarClientes() {
+  const resultadosFacturas = [];
+  for (const currentClient of clients_data) {
+    const CodigoFactura = currentClient.no_factura.split("-");
+    const resultadoFactura = await factura_data(CodigoFactura[0], CodigoFactura[1]);
+    resultadosFacturas.push(resultadoFactura);
+  }
+  return resultadosFacturas;
+}
+
+
+function newPdf(factura){
+
+
+            const recursos = {
+                data: factura.items,
+                css: '/style.css',
+                imgHeader: '/img/factura-header.jpg'
+                }
+            return recursos
+}
+
+         
+procesarClientes()
+  .then((resultadosFacturas) => {
+    const resultados = resultadosFacturas.map(factura => newPdf(factura)).filter(resultado => resultado);
+    //console.log(resultados[0].data)
+    return resultados[0].data;
+  })
+  .then(async (resultados) => { // Convertir el callback en una función asincrónica para usar await
+    app.get('/template', (req, res) => {
+      res.render('index.pug', { resultados });
+    });
   
 
-    (async () => {
+    try { // Manejar los errores de Puppeteer
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
-    
-      // Navigate to the page with the image
+      const fileName = 'ejemplo.pdf'
+
       await page.goto('http://localhost:3000/template');
-    
-      // Generate the PDF with the image included
+
       await page.pdf({
-        path: 'my-pdf-file.pdf',
+        path: fileName,
         width: '820px',
         height: '1370px',
         printBackground: true,
       });
-    
+
       await browser.close();
-    })();
-    
-    
-    
-    
-    let factura = clients_data
-    const recursos = {
-       data: factura.items,
-       css: '/style.css',
-       imgHeader: '/img/factura-header.jpg'
 
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error al generar el PDF');
     }
-
-    const app = express()
-    const port = 3000
-
-    app.get('/', (req, res) => {
-      res.send('Hello World!')
-    })
-
-    app.set('views', './views')
-
-    app.set('view engine', 'pug')
-
-    app.use(express.static('public'))
-
-    app.get('/template', (req, res) => {
-        res.render('index.pug', recursos ); 
-    });
     
-    app.listen(port, () => {
-      console.log(`listening on port ${port}`)
-    })
-
-
-   
-   
-
   
+  });
 
 
-        
+       
--- factura.js
+++ factura.js
@@ -1,8 +1,12 @@
 import fetch  from 'node-fetch';
-
-const response = await fetch('https://webservice.caasd.gob.do:8443/api/factura?id_tipo_trans=FS&id_documento=3512084');
-const clients_data = await response.json();
+import clients_data from './Clientes.js'
 
 
+export async function factura_data(tipo, factura ){
+    const response = await fetch(`https://webservice.caasd.gob.do:8443/api/factura?id_tipo_trans=${tipo}&id_documento=${factura.trim()}`);
+    const factura_data = await response.json();
+    return factura_data;
 
-export default clients_data
+}
+
+
